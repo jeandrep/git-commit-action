1f581997d7fee2bc1c6792dc7fd460098c8e8121
jeandrep <4304398+jeandrep@users.noreply.github.com>
2022-07-29 08:59:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ async function run() {
     const authorName = core.getInput('author_name')
     const authorEmail = core.getInput('author_email')
     var sha = ''
+    var status = ''
     
     await exec('git', [ '-C', workingDirectory, 'init' ])
     await exec('git', [ '-C', workingDirectory, 'add', '-A' ])
     await exec('git', [ '-C', workingDirectory, 'config', '--local', 'user.name', authorName ])
     await exec('git', [ '-C', workingDirectory, 'config', '--local', 'user.email', authorEmail ])
-    await exec('git', [ '-C', workingDirectory, 'commit', '--no-verify', '-m', commitMessage, ' || echo "ignore commit failure, proceed"' ])
+    
+    await exec('git', [ '-C', workingDirectory, 'status', '--porcelain' ], { listeners: { stdout: buffer => status += buffer.toString() }})
+    if (status !== '') {
+      await exec('git', [ '-C', workingDirectory, 'commit', '--no-verify', '-m', commitMessage ])
+    }
     await exec('git', [ '-C', workingDirectory, 'rev-parse', 'HEAD' ], { listeners: { stdout: buffer => sha += buffer.toString() }})
     
     core.setOutput('sha', sha)
